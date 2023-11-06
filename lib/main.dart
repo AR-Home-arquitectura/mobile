794c9a31820c0989c795d1753049c2d8251e715f
Mathyu Cardozo <xmathyu@gmail.com>
2023-11-05 19:28:43 -0500

    firebase working

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+//Firebase imports
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
 
 import 'home_screen.dart';
 
@@ -8,7 +13,9 @@ Future<void> main() async
   try
   {
     WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   }
   catch (errorMsg)
   {
